8d0eff05abb1ad847e6d3be1ee21c4ceb1afbf46
SeniorSSSS <sakariye.sed@outlook.com>
2022-07-21 20:36:33 -0400

    Create To-Do APP basic layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './styles.css';
+
+//The following array holds the 
+let ArrayforProjects = [];
+let ArrayforTodoitems = [];
+
+
+
+//This is a factory used to make "the to-do items" it has the properties & functions the individual objects would have 
+// const monsterFactory = (name,age)) => {
+//     return {
+//         name: name,
+//         age: age,
+//         scare() {
+//             console.log(age);
+//         }
+//     }
+// }
+// To make a specific object call the factory with necessary arguments & assign the return value to a variable.
+// const ghost = monsterFactory('ghouly',251);
